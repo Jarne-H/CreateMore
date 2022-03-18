5f2d550ef6cad9fc4703439b67fca89450781cd9
Cheffren <jeffrey.veyron@gmail.com>
2022-03-18 13:07:41 +0100

    Begin van formulier

--- signUp.php
+++ signUp.php
@@ -26,32 +26,41 @@
 		<div class="liner"></div>
 		<div id="form">
 			<form method="post" action>
-				<input name="email" placeholder="E-mail adres" type="email" required autofocus />
-                <input name="password" placeholder="Wachtwoord" type="password" required />
-                <input name="password_conf" placeholder="Wachtwoord bevestigen" type="password" required />
+			
+				<div>
+                    <label for="email">E-mail</label>
+                    <input name="email" placeholder="E-mail adres" type="email" required autofocus/>
+                </div>
+
+                <div>
+                    <label for="password">Wachtwoord</label>
+                    <input name="password" placeholder="Wachtwoord" type="password" required />
+                </div>
+
+                <div>
+                    <label for="password_conf">Wachtwoord bevestigen</label>
+                    <input name="password_conf" placeholder="Wachtwoord bevestigen" type="password" required />
+                </div>
 				<h5>
 					
 				</h5>
-				<!--input class="btn-toggle btn-toggle-round" id="btn-toggle-1" name="remember" type="checkbox" /><label for="btn-toggle-1"></label><input name="register" type="submit" value="Register" /-->
-			</form>
-		</div>
-		
-	</div>
 
 	
 		<div class="user-messages-area">
 			<div class="alert alert-danger">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				
+				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">Meld je aan</span></button>
 				<ul>
 					<?php //if(isset($error)):?>
 					
-					<?php //endif; ?>
-				</ul>
+
 			</div>
 		</div>
 	
+		<h1>HALLO</h1>
+		
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>/script>
 </body>
 </html>
