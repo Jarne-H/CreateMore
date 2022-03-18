317256d7999d16371284150f2631913714e3451f
Deborah08 <deborahbaeten@hotmail.com>
2022-03-18 12:38:00 +0100

    Update signUp.php

--- signUp.php
+++ signUp.php
@@ -50,6 +50,7 @@
 			</div>
 		</div>
 	
+		<h1>HALLO</h1>
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
