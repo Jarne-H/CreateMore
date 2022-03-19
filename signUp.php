441355caf93f4503563d673512d929a36fa087ba
Cheffren <jeffrey.veyron@gmail.com>
2022-03-19 14:40:49 +0100

    some PHP

--- signUp.php
+++ signUp.php
@@ -5,6 +5,45 @@
 //Als ze leeg zijn --> error true
 //Connectie maken met database
 // email moet op @thomasmore.be eindigen
+
+if (!empty($_POST)) {
+
+	$email = $_POST['email'];
+	$options = ['cost' => 14,];
+	$username = $_POST['username'];
+	$password = $_POST['password'];
+	$passwordconf = $_POST['password_conf'];
+
+		$conn = new PDO('mysql:host=localhost;dbname=createmore', "root", "root");
+		$query  = $conn->prepare("insert into user ( email, username, password, profilepic, savedPostId, likedPostId, toolsId) VALUES ( :email, :username, :password, NULL, NULL, NULL, NULL)
+		");
+		
+
+		if (  $password === $passwordconf) {
+
+
+			$password = password_hash($_POST['password'] . "SDF0303", PASSWORD_DEFAULT, $options);
+			$query ->bindValue(":email", $email);
+			$query ->bindValue(":username", $username);
+
+			$query ->bindValue(":password", $password);
+			$query ->execute();
+			
+
+
+		}
+		else {
+			$error = true;
+		}
+	
+
+
+
+
+
+}
+
+
 ?>
 
 <!DOCTYPE html>
@@ -31,36 +70,50 @@
                     <label for="email">E-mail</label>
                     <input name="email" placeholder="E-mail adres" type="email" required autofocus/>
                 </div>
+				<div class="errorMessage">
+					<p>E-mail moet eindigen op @student.thomasmore.be of @thomasmore.be</p>
+					<p>E-mail bestaat al</p>
+				</div>
+			
+				<div>
+                    <label for="username">Gebruikersnaam</label>
+                    <input name="username" placeholder="Gebruikersnaam" type="text" required/>
+                </div>
+				<div class="errorMessage">
+					<p>Gebruikersnaam mag niet leeg zijn</p>
+				</div>
+			
 
                 <div>
                     <label for="password">Wachtwoord</label>
-                    <input name="password" placeholder="Wachtwoord" type="password" required />
+                    <input name="password" placeholder="Wachtwoord" type="password" required/>
                 </div>
+				<div class="errorMessage">
+					<p>Wachtwoord moet minstens 6 karakters lang zijn.</p>
+					<p>E-mail bestaat al</p>
+				</div>
+			
 
                 <div>
                     <label for="password_conf">Wachtwoord bevestigen</label>
                     <input name="password_conf" placeholder="Wachtwoord bevestigen" type="password" required />
                 </div>
-				<h5>
-					
-				</h5>
-
-	
+				<div class="errorMessage">
+					<p>Wachtwoorden komen niet overeen.</p>
+				</div>
+			
 		<div class="user-messages-area">
 			<div class="alert alert-danger">
 				
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">Meld je aan</span></button>
-				<ul>
+			<input type="submit" value="Meld je aan" class="btn">
+					<ul>
 					<?php //if(isset($error)):?>
 					
 
 			</div>
 		</div>
-	
-		<h1>HALLO</h1>
-		
+		</form>
 
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>/script>
+	
 </body>
 </html>
