1bbc2ec31789b4a9be78ba981eedc89d76e18bd9
Deborah08 <deborahbaeten@hotmail.com>
2022-03-21 16:59:44 +0100

    Update logIn.php

--- logIn.php
+++ logIn.php
@@ -6,9 +6,52 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="css" href="/CSS/style.css">
     <title>Log in</title>
 </head>
 <body>
-    
+<div id="header">
+		<div class="logo"></div>
+	</div>
+	<div id="main">
+		<h1>create More</h1>
+        <h3>Log in</h3>
+		<div class="loginfb"></div>
+		<div class="linel"></div>
+		<div class="liner"></div>
+
+		<div id="form">
+			<form method="post" action="">
+
+				<div class="inputfields">
+                    <label for="username">Gebruikersnaam</label>
+                    <input name="username" placeholder="Gebruikersnaam" type="text" required/>
+                </div>
+
+				<?php if(isset($errorUser)):?>
+				<div class="errorMessage">
+					<p><?php echo $errorUser?></p>
+				</div>
+				<?php endif;?>
+
+
+                <div class="inputfields">
+                    <label for="password">Wachtwoord</label>
+                    <input name="password" placeholder="Wachtwoord" type="password" required/>
+                </div>
+
+				<?php if(isset($errorPass)):?>
+				<div class="errorMessage">
+				    <p><?php echo $errorPass?></p>
+				</div>
+				<?php endif;?>
+
+		
+				<div>
+				<input type="submit" value="Log in" id="btn">
+				</div>
+		</div>
+		</form>
+
 </body>
 </html>
