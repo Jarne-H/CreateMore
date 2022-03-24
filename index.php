bb74bcd6b81ba4a53ef334dfe8045948f0dcadd2
Deborah08 <deborahbaeten@hotmail.com>
2022-03-24 14:18:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+    include_once(__DIR__ . "./includes/nav.inc.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,7 +12,12 @@
 </head>
 
 <body>
-    
+<h1 class="titel">Er zijn nog geen projecten gedeeld</h1>
+    <p class="subtitel">Start met je eerste project te delen aan andere.</p>
+
+    <div class="buttonCenter">
+        <input type="submit" value="Deel project" id="btn">
+    </div>
 </body>
 
 </html>
