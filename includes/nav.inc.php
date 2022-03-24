7e1b535c952a3ec87b8b54df7770268a0ddd3305
Deborah08 <deborahbaeten@hotmail.com>
2022-03-25 00:55:27 +0100

    Update nav.inc.php

--- includes/nav.inc.php
+++ includes/nav.inc.php
@@ -1,4 +1,9 @@
-<!DOCTYPE html>
+<?php
+    session_start();
+    $username = $_SESSION['username'];
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -14,13 +19,12 @@
 
         <input class="search" type="text" placeholder="Zoeken..">
         
-        <p class="welkom">Welkom, gebruiker</p>
+        <p class="welkom">Welkom, <?php echo $username; ?></p>
         <div class="imageCropper">
-            <a href="profile.php"><img src="../assets/profile1.png" alt="profielfoto"></a>
+            <a href="profile.php"><img src="/CreateMore/assets/profile1.png" alt="profielfoto"></a>
         </div>
-
         <a href="logOut.php"><p class="logOut">Uitloggen</p></a>
-
+        
     </nav>
 </body>
 </html>
