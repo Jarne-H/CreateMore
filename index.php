7aacf1ac346f3f77eb06dcff81fbcc9af6f89578
Jarne_H <47690466+jarnejens@users.noreply.github.com>
2022-05-10 11:47:48 +0200

    start van profielpagina

--- index.php
+++ index.php
@@ -8,11 +8,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Starpagina</title>
+    <link rel="stylesheet" href="./CSS/style.css">
 </head>
 
 <body>
-    
+    <a href="logIn.php">Login</a>
+    <a href="signUp.php">signUp</a>
+    <a href="deleteProfile.php">deleteProfile</a>
+    <a href="forgotPassword.php">forgotPassword</a>
+    <a href="requestCode.php">requestCode</a>
+    <a href="profilePage.php">profilePage</a>
 </body>
 
 </html>
---
+++ profilepage.php
@@ -0,0 +1,21 @@
+<?php
+    if(!empty($_GET)){
+        $currentprofilepage = $_GET["profile"];
+    }
+    else{
+        $currentprofilepage = "unknown";
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "CreateMore: " . $currentprofilepage;?></title>
+    <link rel="stylesheet" href="./CSS/style.css">
+</head>
+<body>
+    <p>testerjes</p>
+</body>
+</html>
